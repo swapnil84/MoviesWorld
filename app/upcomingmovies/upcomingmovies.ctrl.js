2cc5359881ba062cfd02eb63ef632465f407aad9
swapnil84 <swapnil.tivlekar@gmail.com>
2016-09-15 23:12:16 -0500

    Add files via upload

--- app/upcomingmovies/upcomingmovies.ctrl.js
+++ app/upcomingmovies/upcomingmovies.ctrl.js
@@ -2,11 +2,12 @@
 	'use strict';
 	angular
 		.module('app.upcomingmovies')
-		.controller('UpcomingMoviesCtrl', UpcomingMoviesCtrl);
+		.controller('upcomingMoviesCtrl', upcomingMoviesCtrl);
 
-	function UpcomingMoviesCtrl($scope, MovieService, $rootScope, $state) {
+	function upcomingMoviesCtrl($scope, movieService, $rootScope, $state) {
 	    var vm = this;
 	    vm.loading = true;
+	    vm.imgpath = 'http://image.tmdb.org/t/p/w500';
 		vm.movies = [];
 		$rootScope.state = $state.current.name;
 		console.log($rootScope.state);
@@ -15,12 +16,10 @@
 		////////////   
 
 		function activate() {
-		    return MovieService.getUpcomingMovies().then(function (data) {
-		        vm.movies = data.results;
-		        vm.imgpath = 'http://image.tmdb.org/t/p/w500';
-			    vm.loading = false;
-			    return vm.movies;
-			});
+		    movieService.getUpcomingMovies().then(function (data) {
+		        vm.movies = data.data.results;
+		        vm.loading = false;
+		    });
 		}
 	}
 
