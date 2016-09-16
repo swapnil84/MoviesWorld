b8565fbd80229bce82775d68e66aa3ceda09cdd5
swapnil84 <swapnil.tivlekar@gmail.com>
2016-09-15 23:11:31 -0500

    Add files via upload

---
+++ app/popularmovies/popularmovies.ctrl.js
@@ -0,0 +1,26 @@
+(function(){
+	'use strict';
+	angular
+		.module('app.popularmovies')
+		.controller('popularMoviesCtrl', popularMoviesCtrl);
+
+	function popularMoviesCtrl($scope, movieService, $rootScope, $state, globalVar) {
+	    var vm = this;
+	    vm.loading = true;
+	    vm.imgpath = globalVar.imgPath;
+		vm.movies = [];
+		$rootScope.state = $state.current.name;
+		console.log($rootScope.state);
+		activate();
+
+	    ////////////
+
+		function activate() {
+		    return movieService.getPopularMovies().then(function (data) {
+		        vm.movies = data.data.results;
+		        vm.loading = false;
+		    });
+		}
+	}
+
+})();
